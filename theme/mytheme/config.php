26c039032ba5559b9d3ef897b52f0e01f50901f3
ArVaganov <ar.vaganov@gmail.com>
2018-03-20 10:34:19 +0500

    add new theme

---
+++ theme/mytheme/config.php
@@ -0,0 +1,10 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$THEME->name = 'mytheme';
+$THEME->parents = array('bootstrapbase');
+
+$THEME->doctype = 'html5';
+$THEME->sheets = array('custom');
+$THEME->enable_dock = true;
+$THEME->editor_sheets = array();
---
+++ theme/mytheme/version.php
@@ -0,0 +1,9 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2017111300;
+$plugin->requires  = 2017110800;
+$plugin->component = 'theme_mytheme';
+$plugin->dependencies = array(
+    'theme_bootstrapbase'  => 2017110800,
+);
